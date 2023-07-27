9357025809baed1ce469dccf62291027de0eea45
JohnDoeTester <tester.donotreply.tester@gmail.com>
2023-07-27 11:57:50 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import Layout from "./components/Layout/Layout";
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { NewsContextProvider } from "./NewsContext";
+import News from "./components/News";
+import "./app.css";
 
 function App() {
-  return <Layout />;
+  return (
+    <NewsContextProvider>
+      <News />
+    </NewsContextProvider>
+  );
 }
 
 export default App;
